8ac2c2940f3b9a8c3ee427cb6e7d98677d7720c1
sajib1066 <sajib1066@gmail.com>
2019-09-01 15:19:05 +0600

    create about page

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_page, name='home')
+    path('', views.home_page, name='home'),
+    path('about', views.about_page, name='about')
 ]
--- personal_portfolio/views.py
+++ personal_portfolio/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'home.html')
+
+def about_page(request):
+    return render(request, 'about.html')
