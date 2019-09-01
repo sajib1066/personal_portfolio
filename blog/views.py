bb33b3c8dc95f65af2fa9caaadcb1ec933aec1da
sajib1066 <sajib1066@gmail.com>
2019-09-01 23:20:30 +0600

    create blog details page

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def blog_page(request):
     return render(request, 'blog/blog.html')
+
+def blog_details(request):
+    return render(request, 'blog/single-blog.html')
