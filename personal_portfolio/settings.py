082626de86772d2efdce811ca6dfaf45b051091d
sajib1066 <sajib1066@gmail.com>
2019-09-01 22:59:32 +0600

    initial postgresql database

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio_db',
+        'USER': 'sajib1066',
+        'PASSWORD': 'sajib1099',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
