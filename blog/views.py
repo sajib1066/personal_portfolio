944d74f3a8e848e9b24a9cb9df67d9932a066b1d
sajib1066 <sajib1066@gmail.com>
2019-09-02 21:14:22 +0600

    show blog post from database

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import AuthorProfile, Category, Tag, Post
 
 
 def blog_page(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.filter(is_draft=False)
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog.html', context)
 
 def blog_details(request):
     return render(request, 'blog/single-blog.html')
