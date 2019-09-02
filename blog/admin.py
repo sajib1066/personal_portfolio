36f8f11ea4966fdaad6dc3618e356806ca566067
sajib1066 <sajib1066@gmail.com>
2019-09-02 11:37:02 +0600

    register and migrate models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.AuthorProfile)
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Post)
