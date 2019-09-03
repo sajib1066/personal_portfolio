97f83346629ec8f0b2422192860833590574e2e8
sajib1066 <sajib1066@gmail.com>
2019-09-03 09:19:49 +0600

    added category models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Portfolio
+from .models import Category, Portfolio
 
 # Register your models here.
+admin.site.register(Category)
 admin.site.register(Portfolio)
