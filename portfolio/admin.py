82f21d22702bfd634b518c88cffc12868fcbd105
sajib1066 <sajib1066@gmail.com>
2019-09-03 12:04:35 +0600

    admin customization

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Category, Portfolio
 
+# admin customization
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'portfolio_type', 'category', 'is_draft', 'date']
+    list_editable = ['title', 'portfolio_type', 'category', 'is_draft']
+    list_filter = ('is_draft', 'portfolio_type')
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Portfolio)
+admin.site.register(Portfolio, PortfolioAdmin)
