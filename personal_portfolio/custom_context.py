d99e410c4d5b75ed2bac38857e7e4bc6af28a0b4
sajib1066 <sajib1066@gmail.com>
2019-09-04 10:10:02 +0600

    add tag context

--- personal_portfolio/custom_context.py
+++ personal_portfolio/custom_context.py
@@ -1,6 +1,7 @@
-from blog.models import Category, Post
+from blog.models import Category, Post, Tag
 
 def category_list(request):
     category = Category.objects.filter(is_draft=False)
     recent_post = Post.objects.filter(is_draft=False).order_by('-pub_date')[:4]
-    return {'category': category, 'r_post': recent_post}
+    tag = Tag.objects.filter(is_draft=False)
+    return {'category': category, 'r_post': recent_post, 'tag': tag}
