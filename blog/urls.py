176a19f320c5125b8684988666ebdc5397119992
sajib1066 <sajib1066@gmail.com>
2019-09-07 15:49:51 +0600

    add blog search option

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_page, name='blog'),
-    path('details/<int:post_id>', views.blog_details, name='blog-details')
+    path('details/<int:post_id>', views.blog_details, name='blog-details'),
+    path('search', views.search_post, name='search')
 ]
